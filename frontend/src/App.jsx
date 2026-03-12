2647458a93e521c45bbc0346a58378c1bab57ed6
Maddie <mirtorabi1234@gmail.com>
2026-03-12 17:26:09 -0400

    1) file explorer button 2) main menu window with exit button on the top right 3) rotating vinyl 4) moving clouds 5) drop sound for clicking on buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
-import {useFloating} from '@floating-ui/react';
-import {useState, useEffect} from 'react';
+import {useState, useRef} from 'react';
 import './App.css'
-import MainWindow from './components/MainWindow';
+import dropSound from "../components/audio/drop_002.ogg";
 
 export default function App() {
-  const [mainWindow, setMainwindow] = useState(false);
-  function click(){
+  const [mainWindow, setMainWindow] = useState(false);
+  const audio = useRef(null);
+return (
+  <div>
+    <audio ref={audio} src={dropSound} />
+    <div className="cloud1"></div>
+    <div className="cloud2"></div>
+    <div className="vinyl rotate-scale-up"></div>
+    <button
+      type="button"
+      className="fileButton"
+      onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}
+    ></button>
 
+    {mainWindow && (
+      <div className="mainWindow">
+          <button type="button" className="exit" onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}></button>
+      </div>
+    )}
 
-  }
-  return (
-    <div className="desktop">
-        <button onClick={() => setMainwindow(!mainWindow)}>
-    </button>
-    {}
-    </div>
-  );
+  </div>
+);
 }
