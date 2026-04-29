38725cb9e8de661ca17455066202a306109d0239
Maddie <mirtorabi1234@gmail.com>
2026-04-28 20:24:43 -0400

    trying to make website responsive (in progress)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,43 @@ export default function App() {
   const [mainWindow, setMainWindow] = useState(false);
   const audio = useRef(null);
 return (
-  <div>
+    <div className="page">
     <audio ref={audio} src={dropSound} />
     <div className="cloud1"></div>
     <div className="cloud2"></div>
     <div className="vinyl rotate-scale-up"></div>
+    <div className="fox"></div>
+    <div className="name"></div>
+
+    <div className="menuButtons">
     <button
       type="button"
-      className="fileButton"
+      className="about"
       onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}
     ></button>
+    <button
+      type="button"
+      className="links"
+      onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}
+    ></button>
+    <button
+      type="button"
+      className="contact"
+      onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}
+    ></button>
+    <button
+      type="button"
+      className="work"
+      onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}
+    ></button>
+    </div>
 
     {mainWindow && (
       <div className="mainWindow">
           <button type="button" className="exit" onClick={() => {audio.current.currentTime = 0; audio.current.play(); setMainWindow(!mainWindow)}}></button>
       </div>
     )}
-
+  
   </div>
 );
 }
